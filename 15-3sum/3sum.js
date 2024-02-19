2378d0b0ba251098ce2812528135fba049addcd4
rajanpreetcan <33518979+rajanpreetcan@users.noreply.github.com>
2024-02-19 11:16:47 -0500

    Time: 168 ms (44.07%) | Memory: 66.8 MB (11.41%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -3,32 +3,31 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-  nums.sort((a, b) => a - b); // Sort the array first
-    const result = [];
-    
-    for (let i = 0; i < nums.length - 2; i++) {
-        if (i === 0 || (i > 0 && nums[i] !== nums[i - 1])) { // Skip duplicates
-            let left = i + 1;
-            let right = nums.length - 1;
-            const target = 0 - nums[i]; // Set the target sum
-            
-            while (left < right) {
-                const sum = nums[left] + nums[right] + nums[i]; // Update sum with nums[i]
-                
-                if (sum === 0) {
-                    result.push([nums[i], nums[left], nums[right]]);
-                    while (left < right && nums[left] === nums[left + 1]) left++; // Skip duplicates
-                    while (left < right && nums[right] === nums[right - 1]) right--; // Skip duplicates
-                    left++;
-                    right--;
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    right--;
-                }
+  
+    let sortedArr = nums.sort((a, b) => a - b)
+    let finalArr = []
+    for (let i = 0; i < sortedArr.length-2; i++) {
+        let left = i+1;
+        let right = sortedArr.length - 1
+        let x = sortedArr[i]
+        if(i > 0 && sortedArr[i] == sortedArr[i-1]){
+            continue;
+        }
+        while (left < right) {
+        let sum = x + sortedArr[left] + sortedArr[right]
+
+            if (sum == 0) {
+                finalArr.push([x, sortedArr[left], sortedArr[right]])
+                while (left < right && nums[left] === nums[left + 1]) left++; // Skip duplicates
+                    while (left < right && nums[right] === nums[right - 1]) right--; 
+                left++
+                right--
+            } else if (sum < 0) {
+                left++
+            } else {
+                right--
             }
         }
     }
-    
-    return result;
+    return finalArr
 };
